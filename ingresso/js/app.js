a3c3c1b16197d3b75fa8a4a73e9565408ff66497
João Davi Mendes Correia <168194655+JoaoDaviMendesCorreia@users.noreply.github.com>
2024-05-16 08:53:23 -0300

    Add files via upload

---
+++ ingresso/js/app.js
@@ -0,0 +1,53 @@
+ 
+ 
+ function comprar () {
+    let tipo = document.getElementById ('tipo-ingresso');
+    let qtd = parseInt(document.getElementById ('qtd').value);
+    alert (tipo)
+    alert (qtd)
+  
+    if (qtd<= 0) {
+        alert ('insira um numero valido')
+        return
+    }
+   else if ('pista' == tipo.value) {
+     comprarPista (qtd)
+   } else if ('superior' == tipo.value) {
+    comprarSuperior (qtd)
+   } else if ('inferior' == tipo.value)
+    comprarinferior (qtd)
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+       
+       let qtdPistatotal = qtdPista - qtd;
+       document.getElementById('qtd-pista').textContent = qtdPistatotal;
+        alert('Compra realizada com sucesso!');
+    }
+}
+function comprarSuperior(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+       
+       let qtdPistatotal = qtdPista - qtd;
+       document.getElementById('qtd-superior').textContent = qtdPistatotal;
+        alert('Compra realizada com sucesso!');
+    }
+}
+function comprarinferior(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+       
+       let qtdPistatotal = qtdPista - qtd;
+       document.getElementById('qtd-inferior').textContent = qtdPistatotal;
+        alert('Compra realizada com sucesso!');
+    }
+}
